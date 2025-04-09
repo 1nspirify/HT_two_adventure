42b4c63a079a08ebd34796704897f17fb8d99f09
Cyrus <159616445+1nspirify@users.noreply.github.com>
2025-04-09 16:05:00 +0600

    mazeScript
    clean

--- Assets/Scripts/Maze/MazeController.cs
+++ Assets/Scripts/Maze/MazeController.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
-using Vector3 = System.Numerics.Vector3;
 
 public class MazeController : MonoBehaviour
 {
